87861e8aa3ca8de1437fc153a2032b03453ec79f
Tshxlxfelx <tsholofelochoma@outlook.com>
2023-02-17 15:34:15 -0800

    C positve, negative or equal

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <istdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main function to generte random number
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+{printf("%d is positive\n", n);
+}
+
+if (n == 0)
+{printf("%d is zero\n", n);
+}
+if (n < 0)
+{printf("%d is negative\n", n);
+}
+
+}
+return (0);
