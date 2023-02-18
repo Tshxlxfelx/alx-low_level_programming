0eaa248fdd0ca1873355419192074da9a0357448
Tshxlxfelx <tsholofelochoma@outlook.com>
2023-02-17 16:04:01 -0800

    positive, negative or equal in C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,19 +9,20 @@
  */
 int main(void)
 {
-        int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        printf("%d ", n);
+int n;
 
-        if (n > 0)
-                printf("is positive\n");
-        else if (n == 0)
-                printf("is zero\n");
-        else
-                printf("is negative\n");
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("%d ", n);
 
-        return (0);
+if (n > 0)
+printf("is positive\n");
+else if (n == 0)
+printf("is zero\n");
+else
+printf("is negative\n");
+
+return (0);
 }
 
