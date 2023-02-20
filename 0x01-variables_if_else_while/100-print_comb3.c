7b9cf17d3e2e69a074dd08099968686c6dfe8f52
Tshxlxfelx <tsholofelochoma@outlook.com>
2023-02-19 20:11:33 -0800

    combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int i, j;
-for (i = 0; i < 9; i++)
+
+for (i = 0; i <= 8; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
@@ -25,4 +27,5 @@ putchar(' ');
 putchar('\n');
 
 return (0);
+}
 
