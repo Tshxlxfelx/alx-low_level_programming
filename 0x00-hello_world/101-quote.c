1e182c112432f3264a8296d4e9079f2e1a0a213b
Tshxlxfelx <tsholofelochoma@outlook.com>
2023-02-19 19:23:07 -0800

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+const char *str = "and that piece of art is useful\"-Dora Korpar,2015-10-19\n";
+ssize_t len = 0;
+
+while (str[len] != '\0')
+{
+len++;
+}
+
+write(2, str, len);
+
+return (1);
+}
+
