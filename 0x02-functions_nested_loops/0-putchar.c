1583d6bd012b1a446507b352aa94f93b6ec199da
Tshxlxfelx <tsholofelochoma@outlook.com>
2023-02-21 06:24:16 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include " main.h"
 
 /**
  *main - prints _putchar
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-putchar('');
+putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
