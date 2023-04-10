be2d6d0776631cb136e1778078a45b4e6d4c6767
Tshxlxfelx <tsholofelochoma@outlook.com>
2023-04-10 03:36:27 -0700

    function that creates a file.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,8 +11,7 @@ int _atoi(char *s)
 int sign = 1;
 unsigned int num = 0;
 
-do
-{
+do {
 if (*s == '-')
 sign *= -1;
 else if (*s >= '0' && *s <= '9')
@@ -20,8 +19,7 @@ num = (num * 10) + (*s - '0');
 else if (num > 0)
 break;
 
-}
-while (*s++);
+} while (*s++);
 
 return (num * sign);
 }
