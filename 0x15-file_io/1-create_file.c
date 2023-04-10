d089588ce77dee793e4e9ab913581cbcf64d9631
Tshxlxfelx <tsholofelochoma@outlook.com>
2023-04-10 04:17:01 -0700

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,42 @@
+#include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 /**
- * create_file - creates a file with a fiven name
- * and writes given text to it
- * @filename: name of the file to create
- * @text_content: a NULL-terminated string to write the file
+ * create_file - creates a file and writes text to it
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
  *
- * Return: 1 success, -1 failure
+ * Return: 1 on success, -1 on failure
  */
-
-int create_file(const char *filename, char *text_content);
-
+int create_file(const char *filename, char *text_content)
 {
-int fd, res, len
-if (!filename)
+int fd, len = 0, wrote;
+
+if (filename == NULL)
 return (-1);
+
+if (text_content != NULL)
+{
+while (text_content[len])
+len++;
+}
 
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (fd < 0)
+if (fd == -1)
 return (-1);
 
-if (!text_content)
-text_content = "";
-
-for (len = 0; text_content[len]; len++);
-res = write(fd, text_content, len);
-if (res < 0)
-
+if (len > 0)
+{
+wrote = write(fd, text_content, len);
+if (wrote == -1)
 {
 close(fd);
 return (-1);
 }
+}
+
 close(fd);
 return (1);
 }
+
